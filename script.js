19916dbce0a9db436237e5a8a144a32baefac1ef
harshit-jain52 <harshitjain0502@gmail.com>
2024-04-11 17:59:38 +0530

    Code Formatting

--- script.js
+++ script.js
@@ -1,171 +1,169 @@
-const list = document.querySelector('#list ul');
+const list = document.querySelector("#list ul");
 
 const listOut = () => {
-    let keys = Object.keys(localStorage);
+  let keys = Object.keys(localStorage);
 
-    let todoList = [];
-    keys.forEach(key => {
-        if (key.substring(0, 6) == 'hjtodo') {
-            todoList.push(key.slice(7));
-        }
-    })
-
-    todoList.sort();
-    while (list.firstChild) {
-        list.removeChild(list.lastChild);
+  let todoList = [];
+  keys.forEach((key) => {
+    if (key.substring(0, 6) == "hjtodo") {
+      todoList.push(key.slice(7));
     }
-    todoList.forEach(todo => addToDo(todo));
-}
+  });
+
+  todoList.sort();
+  while (list.firstChild) {
+    list.removeChild(list.lastChild);
+  }
+  todoList.forEach((todo) => addToDo(todo));
+};
 
 listOut();
 
-document.addEventListener('click', () => {
-    list.childNodes.forEach(child => {
-        if (child.className == 'editing' && document.activeElement.parentElement !== child) {
-            exitEdit(child);
-        }
-    })
+document.addEventListener("click", () => {
+  list.childNodes.forEach((child) => {
+    if (
+      child.className == "editing" &&
+      document.activeElement.parentElement !== child
+    ) {
+      exitEdit(child);
+    }
+  });
 });
 
-document.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-        if (document.activeElement.parentElement.className == 'editing') {
-            acceptEdit(document.activeElement.parentElement);
-        }
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    if (document.activeElement.parentElement.className == "editing") {
+      acceptEdit(document.activeElement.parentElement);
     }
-})
-
-list.addEventListener('click', e => {
-    let li = null;
-
-    if (e.target.className == 'delete') {
-        li = e.target.parentElement;
-        deleteToDo(li);
-    }
-    else if (e.target.className == 'delete-ico') {
-        li = e.target.parentElement.parentElement;
-        deleteToDo(li);
-    }
-    else if (e.target.className == 'edit') {
-        li = e.target.parentElement;
-        editToDo(li);
-    }
-    else if (e.target.className == 'edit-ico') {
-        li = e.target.parentElement.parentElement;
-        editToDo(li);
-    }
-    else if (e.target.className == 'cancel') {
-        li = e.target.parentElement;
-        exitEdit(li)
-    }
-    else if (e.target.className == 'cancel-ico') {
-        li = e.target.parentElement.parentElement;
-        exitEdit(li)
-    }
-    else if (e.target.className == 'done') {
-        li = e.target.parentElement;
-        acceptEdit(li)
-    }
-    else if (e.target.className == 'done-ico') {
-        li = e.target.parentElement.parentElement;
-        acceptEdit(li)
-    }
+  }
 });
 
-const add = document.forms['add-todo'];
-add.addEventListener('submit', e => {
-    e.preventDefault();
-    const newToDo = add.querySelector('input[type="text"]').value;
-    add.querySelector('input[type="text"]').value = "";
-    if (newToDo) localStorage.setItem(`hjtodo.${capitalizeFirstLetter(newToDo.trim())}`, 1);
-    listOut();
+list.addEventListener("click", (e) => {
+  let li = null;
+
+  if (e.target.className == "delete") {
+    li = e.target.parentElement;
+    deleteToDo(li);
+  } else if (e.target.className == "delete-ico") {
+    li = e.target.parentElement.parentElement;
+    deleteToDo(li);
+  } else if (e.target.className == "edit") {
+    li = e.target.parentElement;
+    editToDo(li);
+  } else if (e.target.className == "edit-ico") {
+    li = e.target.parentElement.parentElement;
+    editToDo(li);
+  } else if (e.target.className == "cancel") {
+    li = e.target.parentElement;
+    exitEdit(li);
+  } else if (e.target.className == "cancel-ico") {
+    li = e.target.parentElement.parentElement;
+    exitEdit(li);
+  } else if (e.target.className == "done") {
+    li = e.target.parentElement;
+    acceptEdit(li);
+  } else if (e.target.className == "done-ico") {
+    li = e.target.parentElement.parentElement;
+    acceptEdit(li);
+  }
 });
 
-const deleteToDo = li => {
-    list.removeChild(li);
-    localStorage.removeItem(`hjtodo.${li.firstChild.textContent}`);
-    listOut();
-}
+const add = document.forms["add-todo"];
+add.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newToDo = add.querySelector('input[type="text"]').value;
+  add.querySelector('input[type="text"]').value = "";
+  if (newToDo)
+    localStorage.setItem(`hjtodo.${capitalizeFirstLetter(newToDo.trim())}`, 1);
+  listOut();
+});
+
+const deleteToDo = (li) => {
+  list.removeChild(li);
+  localStorage.removeItem(`hjtodo.${li.firstChild.textContent}`);
+  listOut();
+};
 
 function addToDo(newToDo) {
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    const deleteBtn = document.createElement('button');
-    const editBtn = document.createElement('button');
-    const deleteImg = document.createElement('img');
-    const editImg = document.createElement('img');
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const deleteBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
+  const deleteImg = document.createElement("img");
+  const editImg = document.createElement("img");
 
-    deleteImg.classList.add('delete-ico');
-    deleteImg.setAttribute('src', "icons/trash.png");
-    deleteBtn.classList.add('delete');
-    deleteBtn.classList.add('button');
-    deleteBtn.appendChild(deleteImg);
+  deleteImg.classList.add("delete-ico");
+  deleteImg.setAttribute("src", "icons/trash.png");
+  deleteBtn.classList.add("delete");
+  deleteBtn.classList.add("button");
+  deleteBtn.appendChild(deleteImg);
 
-    editImg.classList.add('edit-ico');
-    editImg.setAttribute('src', "icons/pencil-line.png")
-    editBtn.classList.add('edit');
-    editBtn.classList.add('button');
-    editBtn.appendChild(editImg);
+  editImg.classList.add("edit-ico");
+  editImg.setAttribute("src", "icons/pencil-line.png");
+  editBtn.classList.add("edit");
+  editBtn.classList.add("button");
+  editBtn.appendChild(editImg);
 
-    span.textContent = `${newToDo}`;
-    li.appendChild(span);
-    li.appendChild(editBtn);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
+  span.textContent = `${newToDo}`;
+  li.appendChild(span);
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 }
 
-const editToDo = li => {
-    let oldText = li.firstElementChild.textContent;
-    while (li.firstChild) {
-        li.removeChild(li.lastChild);
-    }
-    li.className = 'editing';
+const editToDo = (li) => {
+  let oldText = li.firstElementChild.textContent;
+  while (li.firstChild) {
+    li.removeChild(li.lastChild);
+  }
+  li.className = "editing";
 
-    const span = document.createElement('span');
-    const input = document.createElement('textarea');
-    const cancelBtn = document.createElement('button');
-    const doneBtn = document.createElement('button');
-    const cancelImg = document.createElement('img');
-    const doneImg = document.createElement('img');
+  const span = document.createElement("span");
+  const input = document.createElement("textarea");
+  const cancelBtn = document.createElement("button");
+  const doneBtn = document.createElement("button");
+  const cancelImg = document.createElement("img");
+  const doneImg = document.createElement("img");
 
-    span.textContent = oldText;
-    span.style.display = 'none';
+  span.textContent = oldText;
+  span.style.display = "none";
 
-    input.textContent = oldText;
+  input.textContent = oldText;
 
-    cancelImg.classList.add('cancel-ico');
-    cancelImg.setAttribute('src', "icons/cancel.png");
-    cancelBtn.classList.add('cancel');
-    cancelBtn.classList.add('button');
-    cancelBtn.appendChild(cancelImg);
+  cancelImg.classList.add("cancel-ico");
+  cancelImg.setAttribute("src", "icons/cancel.png");
+  cancelBtn.classList.add("cancel");
+  cancelBtn.classList.add("button");
+  cancelBtn.appendChild(cancelImg);
 
-    doneImg.classList.add('done-ico');
-    doneImg.setAttribute('src', "icons/check.png")
-    doneBtn.classList.add('done');
-    doneBtn.classList.add('button');
-    doneBtn.appendChild(doneImg);
+  doneImg.classList.add("done-ico");
+  doneImg.setAttribute("src", "icons/check.png");
+  doneBtn.classList.add("done");
+  doneBtn.classList.add("button");
+  doneBtn.appendChild(doneImg);
 
-    li.append(span);
-    li.appendChild(input);
-    li.appendChild(doneBtn);
-    li.appendChild(cancelBtn);
+  li.append(span);
+  li.appendChild(input);
+  li.appendChild(doneBtn);
+  li.appendChild(cancelBtn);
 
-    input.focus();
-}
+  input.focus();
+};
 
-const exitEdit = li => {
-    list.removeChild(li);
-    listOut();
-}
+const exitEdit = (li) => {
+  list.removeChild(li);
+  listOut();
+};
 
-const acceptEdit = li => {
-    let newText = li.childNodes[1].value;
-    localStorage.removeItem(`hjtodo.${li.firstChild.textContent}`);
-    if (newText) localStorage.setItem(`hjtodo.${capitalizeFirstLetter(newText.trim())}`, 1)
-    li.firstChild.textContent = newText;
-    listOut();
-}
+const acceptEdit = (li) => {
+  let newText = li.childNodes[1].value;
+  localStorage.removeItem(`hjtodo.${li.firstChild.textContent}`);
+  if (newText)
+    localStorage.setItem(`hjtodo.${capitalizeFirstLetter(newText.trim())}`, 1);
+  li.firstChild.textContent = newText;
+  listOut();
+};
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
